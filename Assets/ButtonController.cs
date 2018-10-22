e3fcda67c5a26cdcc4134ff11b55e030d78a88cf
Kei Sugano <tobasojyo@gmail.com>
2018-10-22 17:27:28 +0900

    can record the japanese voice quickly

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -9,39 +9,75 @@ using IBM.Watson.DeveloperCloud.Connection;
 public class ButtonController : MonoBehaviour {
     [SerializeField]
     private SpeechToText m_SpeechToText;
+    
+    public AudioClip audio;
+    public AudioSource audioSource;
 
-    string cre_id  = "2edf7f72-225b-4692-832b-abb07870ac18";                  // 資格情報より
-    string cre_pw  = "ppa81Jdk6PTx";                                          // 資格情報より
-    string cre_url = "https://stream.watsonplatform.net/speech-to-text/api";  // 資格情報より
+    string cre_id        = "2edf7f72-225b-4692-832b-abb07870ac18";                  
+    string cre_pw        = "ppa81Jdk6PTx";                                          
+    string cre_url       = "https://stream.watsonplatform.net/speech-to-text/api";
+    bool   isSoundPlayed = false;
+    float MAXIMUM_LENGTH = 5.0f;
+
+    void Awake(){
+        this.audioSource = GetComponent<AudioSource>();
+    }
 
     IEnumerator Start(){
-        Credentials credentials = new Credentials(cre_id, cre_pw, cre_url);
-        m_SpeechToText = new SpeechToText(credentials);
-        m_SpeechToText.Keywords = new string[] { "ibm" };
+        Debug.Log("Start");
+        Credentials credentials          = new Credentials(cre_id, cre_pw, cre_url);
+        m_SpeechToText                   = new SpeechToText(credentials);
+        m_SpeechToText.Keywords          = new string[] { "ibm" };
         m_SpeechToText.KeywordsThreshold = 0.1f;
 
-        // 音声をマイクから 3 秒間取得する
-        Debug.Log("Start record"); //集音開始
-        Debug.Log("Start record"); //集音開始
-        Debug.Log("Start record"); //集音開始
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("Start record");
+        this.audioSource.Play();
+        yield return new WaitForSeconds(0.1f);
 
         var audioSource  = GetComponent<AudioSource>();
+
         audioSource.clip = Microphone.Start(null, true, 10, 44100);
         audioSource.loop = false;
         audioSource.spatialBlend = 0.0f;
-        yield return new WaitForSeconds(5f);
-        Microphone.End(null); //集音終了
-        Debug.Log("Finish record");
+        
+        yield return new WaitForSeconds(MAXIMUM_LENGTH);
 
+        Microphone.End(null);
+        Debug.Log("Finish record");
+    
         Debug.Log("Playing");
-        // ためしに録音内容を再生してみる
         audioSource.Play();
         Debug.Log("Play Finished");
-
+        yield return new WaitForSeconds(1f);
+    
         // SpeechToText を日本語指定して、録音音声をテキストに変換
         m_SpeechToText.RecognizeModel = "ja-JP_BroadbandModel";
         m_SpeechToText.Recognize(HandleOnRecognize, OnFail, audioSource.clip);
+
+        yield return new WaitForSeconds(1f);
     }
+
+    //IEnumerator Start(){
+
+    //        audioSource.clip = Microphone.Start(null, true, 10, 44100);
+    //        audioSource.loop = false;
+    //        audioSource.spatialBlend = 0.0f;
+    //        yield return new WaitForSeconds(5f);
+    //        Microphone.End(null); //集音終了
+    //        Debug.Log("Finish record");
+    //
+    //        Debug.Log("Playing");
+    //        // ためしに録音内容を再生してみる
+    //        audioSource.Play();
+    //        Debug.Log("Play Finished");
+    //
+    //        // SpeechToText を日本語指定して、録音音声をテキストに変換
+    //        m_SpeechToText.RecognizeModel = "ja-JP_BroadbandModel";
+    //        m_SpeechToText.Recognize(HandleOnRecognize, OnFail, audioSource.clip);
+    //    }
+    //}
 
     void Update(){
     //    Button button = GameObject.Find("Button").GetComponent<Button>();
